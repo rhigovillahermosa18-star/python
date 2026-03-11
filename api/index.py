a7147f43c3ceac7ee132a39c6eed7b8bda3df7c6
rhigovillahermosa18-star <rhigovillahermosa18@gmail.com>
2026-03-11 14:22:20 +0800

    update project files

--- api/index.py
+++ api/index.py
@@ -1,17 +1,59 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__, template_folder="../templates")
+app.secret_key = "secret123"
 
 inventory = []
 next_id = 1
 
+# simple login credentials
+USERNAME = "admin"
+PASSWORD = "1234"
+
+
+# LOGIN PAGE
+@app.route("/login", methods=["GET","POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == USERNAME and password == PASSWORD:
+            session["user"] = username
+            return redirect(url_for("index"))
+        else:
+            return render_template("login.html", error="Invalid Login")
+
+    return render_template("login.html")
+
+
+# LOGOUT
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
+# HOME PAGE
 @app.route("/")
 def index():
+
+    if "user" not in session:
+        return redirect(url_for("login"))
+
     total = sum(float(item["price"]) for item in inventory)
     return render_template("index.html", items=inventory, total=total)
 
+
+# ADD
 @app.route("/add", methods=["GET","POST"])
 def add():
+
+    if "user" not in session:
+        return redirect(url_for("login"))
+
     global next_id
 
     if request.method == "POST":
@@ -31,14 +73,27 @@ def add():
 
     return render_template("add.html")
 
+
+# DELETE
 @app.route("/delete/<int:id>")
 def delete(id):
+
+    if "user" not in session:
+        return redirect(url_for("login"))
+
     global inventory
     inventory = [item for item in inventory if item["id"] != id]
+
     return redirect(url_for("index"))
 
+
+# EDIT
 @app.route("/edit/<int:id>", methods=["GET","POST"])
 def edit(id):
+
+    if "user" not in session:
+        return redirect(url_for("login"))
+
     item = next((x for x in inventory if x["id"] == id), None)
 
     if request.method == "POST":
@@ -51,6 +106,3 @@ def edit(id):
         return redirect(url_for("index"))
 
     return render_template("edit.html", item=item)
-
-# IMPORTANT for Vercel
-app = app
